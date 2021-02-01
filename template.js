54b02012d3a9d509aef1dd4f64a134a7d5b52736
Avishek Majumder <avishekmojo212@gmail.com>
2021-02-01 21:27:36 +0600

    done for now

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy`;
+
+const multiLine = `I love you
+I miss you
+no. I dont need you
+BAily road e dorkar nai`
+
+console.log(multiLine);
